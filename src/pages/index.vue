985efc520173cc39cf423345b3861a4ee2c4b9d5
坂本一弥 <sakamotokazuya@sakamotokazuyanoMacBook-Pro.local>
2023-08-24 00:49:55 +0900

    add port HMR

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { reactive, computed } from 'vue'
+
+type State = {
+  text: string
+}
+
+const initialState = (): State => ({
+  text: "kinako",
+})
+const state = reactive<State>(initialState())
+
+
+</script>
+
+<template>
+  <div>
+    {{ state.text }}
+  </div>
+</template>
+
+
+
+<style>
+
+</style>
