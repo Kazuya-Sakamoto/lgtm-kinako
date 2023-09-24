c2673b43a3ab498979a2a1bb1522708e1f38ce93
坂本一弥 <sakamotokazuya@sakamotokazuyanoMacBook-Pro.local>
2023-09-24 21:13:35 +0900

    edit favicon

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -2,6 +2,18 @@
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "LGTM-kinako",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/png", href: "/favicon.png" }, // これを追記する
+      ],
+    },
+  },
   alias: {
     // "@src": path.resolve(__dirname, "../src"),
   },
