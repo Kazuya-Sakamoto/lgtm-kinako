fbab6e231b0e7a9d71bfed0ef0e70f2042c1bc5d
坂本一弥 <sskazuya1130@gmail.com>
2023-10-15 15:47:04 +0900

    seo

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -7,7 +7,30 @@ export default defineNuxtConfig({
       title: "LGTM-kinako きなこ（犬）のLGTM画像",
       meta: [
         { charset: "utf-8" },
+        {
+          name: "description",
+          content:
+            "LGTM-kinakoは、きなこ、犬、わんこ、わんちゃんのLGTM画像を自由にダウンロードして使用できるサービスです。GitHubでプルリクエストをレビューした際にLooks Good To Meの略でLGTMと書くことがあります。このLGTM画像にきなこの画像を使ってみてください。LGTM-kinakoは、様々なエンジニアたちがLGTMを送り合うことができます。",
+        },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { property: "og:type", content: "website" },
+        { property: "og:site_name", content: "LGTM-kinako" },
+        {
+          property: "og:image",
+          content: "https://d18g0hf2wnz3gs.cloudfront.net/favicon.jpg",
+        },
+        {
+          property: "og:url",
+          hid: "og:url",
+          content: "https://lgtm-kinako.com/",
+        },
+        { hid: "og:title", property: "og:title", content: "LGTM-kinako" },
+        {
+          property: "og:description",
+          content:
+            "LGTM-kinakoは、きなこ、犬、わんこ、わんちゃんのLGTM画像を自由にダウンロードして使用できるサービスです。GitHubでプルリクエストをレビューした際にLooks Good To Meの略でLGTMと書くことがあります。このLGTM画像にきなこの画像を使ってみてください。LGTM-kinakoは、様々なエンジニアたちがLGTMを送り合うことができます。",
+        },
+        { name: "twitter:card", content: "summary" },
       ],
       link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
