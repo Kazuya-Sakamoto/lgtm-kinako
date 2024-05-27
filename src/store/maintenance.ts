ee5e378113ac3244e5962c50c85e5251b8a09bd7
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2024-05-27 09:17:45 +0900

    Revert "メンテナンスモードの機構を作成する (#54)" (#55)
    This reverts commit 5c8e9fea06b4a53bf09950239a8c1aff6e3d45bc.

--- src/store/maintenance.ts
+++ src/store/maintenance.ts
@@ -4,16 +4,23 @@ import { ref } from 'vue'
 export const useMaintenanceStore = defineStore(
   'maintenance',
   () => {
+    const done = ref(false)
     const show = ref(false)
-    return {
-      show,
-      openDialog: () => {
-        return (show.value = true)
-      },
-      closeDialog: () => {
-        return (show.value = false)
-      },
+
+    const openDialog = () => {
+      show.value = true
+      // if (done.value) return
+      // 計測の目処とかが立ったら、再度検討する
+      // https://github.com/Kazuya-Sakamoto/lgtm-kinako/issues/25
+      // show.value = true;
     }
+
+    const closeDialog = () => {
+      show.value = false
+      done.value = true
+    }
+
+    return { done, show, openDialog, closeDialog }
   },
   {
     persist: true,
